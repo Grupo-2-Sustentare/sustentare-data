587782c3ab3f7a98aeae2f3b5841b0740279bdab
PereiraGus <gustavo.oito@hotmail.com>
2024-10-29 18:04:37 -0300

    Correção nas categorias de população.

--- 3_DML_Popula.sql
+++ 3_DML_Popula.sql
@@ -7,15 +7,15 @@ INSERT INTO usuario (nome, email, senha, acesso, ativo) VALUES
 -- ============================================================== Audit usuário =============================================================================
 -- Inserts para criação dos 8 usuários
 /*INSERT INTO usuario_audit (descricao, dataHora, responsavel, usuarioAlterado) VALUES
-('Criação de usuário', '2024-05-25 08:00:00', 100, 101),
-('Criação de usuário', '2024-05-25 08:10:00', 100, 102),
-('Criação de usuário', '2024-05-25 08:20:00', 100, 103),
+('Criação de usuário', '2024-07-25 08:00:00', 100, 101),
+('Criação de usuário', '2024-07-25 08:10:00', 100, 102),
+('Criação de usuário', '2024-07-25 08:20:00', 100, 103),
 
 -- Inserts para 3 registros de alteração
 INSERT INTO usuario_audit (descricao, dataHora, responsavel, usuarioAlterado) VALUES
-('Alteração de senha', '2024-05-26 14:30:00', 100, 100),
-('Alteração de email', '2024-05-27 09:15:00', 100, 101),
-('Atualização de perfil de acesso', '2024-05-28 16:45:00', 100, 102);
+('Alteração de senha', '2024-07-26 14:30:00', 100, 100),
+('Alteração de email', '2024-07-27 09:15:00', 100, 101),
+('Atualização de perfil de acesso', '2024-07-28 16:45:00', 100, 102);
 */
 -- ============================================================ Unidade de medida =============================================================================
 
@@ -31,11 +31,11 @@ INSERT INTO unidade_medida (categoria, conversao_padrao, nome, simbolo, ativo) V
 -- ========================================================== Unidades de medida audit ==========================================================================
 /*
 INSERT INTO unidade_medida_audit (descricao, dataHora, fkUsuario, fkUnidadeMedida) VALUES
-('Litro', '2024-05-25 10:00:00', 100, 1),
-('Mililitro', '2024-05-25 10:10:00', 100, 2),
-('Quilograma', '2024-05-25 10:20:00', 100, 3),
-('Grama', '2024-05-25 10:30:00', 100, 4),
-('Unidade', '2024-05-25 11:10:00', 100, 8),
+('Litro', '2024-07-25 10:00:00', 100, 1),
+('Mililitro', '2024-07-25 10:10:00', 100, 2),
+('Quilograma', '2024-07-25 10:20:00', 100, 3),
+('Grama', '2024-07-25 10:30:00', 100, 4),
+('Unidade', '2024-07-25 11:10:00', 100, 8),
 */
 -- =================================================================== Categoria ===============================================================================
 
@@ -51,7 +51,7 @@ INSERT INTO categoria_item (nome, ativo) VALUES
 -- =============================================================== Categoria Audit ================================================================================
 /*
 INSERT INTO categoria_item_audit (descricao, dataHora, fkCategoriaItem, fkUsuario) VALUES
-('Criação da categoria: Doces por encomenda', '2024-05-25 10:00:00', 14, 100);
+('Criação da categoria: Doces por encomenda', '2024-07-25 10:00:00', 14, 100);
 */
 -- ==================================================================== Item =====================================================================================
 
@@ -80,9 +80,9 @@ INSERT INTO item (fk_categoria_item, nome, perecivel, fk_unidade_medida, dias_ve
 -- ==================================================================== Item audit =================================================================================
 /*
 INSERT INTO item_audit (descricao, dataHora, fkItem, fkUsuario) VALUES
-('Adição de Detergente', '2024-01-15 10:30:00', 1, 100), -- João Silva adicionou Detergente
-('Adição de Sacola plástica', '2024-01-16 11:00:00', 2, 101), -- Maria Souza adicionou Sacola plástica
-('Adição de Heineken garrafa', '2024-01-17 09:45:00', 3, 102), -- Carlos Pereira adicionou Cerveja
+('Entrada de Detergente', '2024-08-15 10:30:00', 1, 100), -- João Silva adicionou Detergente
+('Entrada de Sacola plástica', '2024-08-16 11:00:00', 2, 101), -- Maria Souza adicionou Sacola plástica
+('Entrada de Heineken garrafa', '2024-08-17 09:45:00', 3, 102), -- Carlos Pereira adicionou Cerveja
 */
 -- ================================================================== Produto =============================================================================
 
@@ -107,29 +107,29 @@ INSERT INTO produto (fk_item, preco, qtd_produto, qtd_medida, ativo) VALUES
 -- ============================================================= Produto Audit ======================================================================
 /*
 INSERT INTO produto_audit (descricao, dataHora, fkUsuario, fkProduto, fkItem) VALUES
-('Adição produto OMO 500ml', NOW(), 100, 1, 1), -- João Silva adicionou Detergente OMO
-('Adição produto Sacolas Carrefour', NOW(), 101, 2, 2), -- Maria Souza adicionou Sacolas Carrefour
-('Adição produto Cerveja Skol', NOW(), 102, 3, 3), -- Carlos Pereira adicionou Cerveja Skol
-('Adição produto Choc Nestlé 200g', NOW(), 103, 4, 4), -- Ana Oliveira adicionou Chocolate Nestlé
-('Adição produto Panela Tramontina', NOW(), 104, 5, 5), -- Paulo Santos adicionou Panela Tramontina
-('Adição produto Copo Copobrás', NOW(), 105, 6, 6), -- Fernanda Lima adicionou Copo Copobrás
-('Adição produto Molho Heinz 340g', NOW(), 106, 7, 7), -- Ricardo Alves adicionou Molho Heinz
-('Adição produto Frango Seara 1kg', NOW(), 107, 8, 8), -- Beatriz Ramos adicionou Frango Seara
-('Adição produto Camarão Qualitá', NOW(), 100, 9, 9), -- João Silva adicionou Camarão Qualitá
-('Adição produto Arroz Tio João 5kg', NOW(), 101, 10, 10), -- Maria Souza adicionou Arroz Tio João
-('Adição produto Arroz Camil 5kg', NOW(), 101, 11, 10), -- Maria Souza adicionou Arroz Camil
-('Adição produto Cenoura Orgânica', NOW(), 102, 12, 11), -- Carlos Pereira adicionou Cenoura Orgânica
-('Adição produto Maçã Fuji 1kg', NOW(), 103, 13, 12), -- Ana Oliveira adicionou Maçã Fuji
-('Adição produto Queijo Tirol 500g', NOW(), 104, 14, 13), -- Paulo Santos adicionou Queijo Tirol
-('Adição produto Sorvete Kibon 1L', NOW(), 105, 15, 14), -- Fernanda Lima adicionou Sorvete Kibon
-('Adição produto Bolo Bauducco 1 un', NOW(), 106, 16, 15); -- Ricardo Alves adicionou Bolo Bauducco
+('Entrada produto OMO 500ml', NOW(), 100, 1, 1), -- João Silva adicionou Detergente OMO
+('Entrada produto Sacolas Carrefour', NOW(), 101, 2, 2), -- Maria Souza adicionou Sacolas Carrefour
+('Entrada produto Cerveja Skol', NOW(), 102, 3, 3), -- Carlos Pereira adicionou Cerveja Skol
+('Entrada produto Choc Nestlé 200g', NOW(), 103, 4, 4), -- Ana Oliveira adicionou Chocolate Nestlé
+('Entrada produto Panela Tramontina', NOW(), 104, 5, 5), -- Paulo Santos adicionou Panela Tramontina
+('Entrada produto Copo Copobrás', NOW(), 105, 6, 6), -- Fernanda Lima adicionou Copo Copobrás
+('Entrada produto Molho Heinz 340g', NOW(), 106, 7, 7), -- Ricardo Alves adicionou Molho Heinz
+('Entrada produto Frango Seara 1kg', NOW(), 107, 8, 8), -- Beatriz Ramos adicionou Frango Seara
+('Entrada produto Camarão Qualitá', NOW(), 100, 9, 9), -- João Silva adicionou Camarão Qualitá
+('Entrada produto Arroz Tio João 5kg', NOW(), 101, 10, 10), -- Maria Souza adicionou Arroz Tio João
+('Entrada produto Arroz Camil 5kg', NOW(), 101, 11, 10), -- Maria Souza adicionou Arroz Camil
+('Entrada produto Cenoura Orgânica', NOW(), 102, 12, 11), -- Carlos Pereira adicionou Cenoura Orgânica
+('Entrada produto Maçã Fuji 1kg', NOW(), 103, 13, 12), -- Ana Oliveira adicionou Maçã Fuji
+('Entrada produto Queijo Tirol 500g', NOW(), 104, 14, 13), -- Paulo Santos adicionou Queijo Tirol
+('Entrada produto Sorvete Kibon 1L', NOW(), 105, 15, 14), -- Fernanda Lima adicionou Sorvete Kibon
+('Entrada produto Bolo Bauducco 1 un', NOW(), 106, 16, 15); -- Ricardo Alves adicionou Bolo Bauducco
 */
 -- ====================================================================== Fechamento ====================================================================
 
 -- Inserts para fechamento_estoque
 /*
 INSERT INTO fechamento_estoque (data_fim, data_inicio, data_fechamento, is_manual) VALUES
-('2024-05-01 00:00:00', '2024-04-01 00:00:00', '2024-05-01 00:00:00', 0),
+('2024-07-01 00:00:00', '2024-04-01 00:00:00', '2024-07-01 00:00:00', 0),
 ('2024-09-01 00:00:00', '2024-08-01 00:00:00', '2024-09-01 00:00:00', 1);
 */
 -- ====================================================================== Fechamento Audit ====================================================================
@@ -143,76 +143,76 @@ INSERT INTO fechamento_estoque_audit (descricao, dataHora, fkUsuario, fkFechamen
 
 -- Inserts para interacao_estoque
 INSERT INTO interacao_estoque (fk_produto, data_hora, fk_fechamento_estoque, categoria_interacao) VALUES
-(1, '2024-05-10 08:30:00', 1, 'Adição'), -- Adicionando Detergente OMO
-(3, '2024-05-15 14:45:00', 1, 'Adição'), -- Adicionando Cerveja Skol
-(10, '2024-05-20 10:15:00', 1, 'Retirada'), -- Retirando Arroz Tio João
-(11, '2024-05-25 11:30:00', 1, 'Retirada'), -- Retirando Arroz Camil
-(12, '2024-05-30 09:00:00', 1, 'Adição'); -- Adicionando Cenoura Orgânica
+(1, '2024-07-10 08:30:00', 1, 'Entrada'), -- Adicionando Detergente OMO
+(3, '2024-07-15 14:45:00', 1, 'Entrada'), -- Adicionando Cerveja Skol
+(10, '2024-07-20 10:15:00', 1, 'Saída'), -- Retirando Arroz Tio João
+(11, '2024-07-25 11:30:00', 1, 'Saída'), -- Retirando Arroz Camil
+(12, '2024-07-30 09:00:00', 1, 'Entrada'); -- Adicionando Cenoura Orgânica
 
 /*
 -- Inserts para interacao_estoque_audit
 INSERT INTO interacao_estoque_audit (descricao, dataHora, fkUsuario, fkInteracaoEstoque, fkProduto) VALUES
-('Adição de Detergente OMO', '2024-05-10 08:30:00', 100, 1, 1), -- João Silva adicionou Detergente OMO
-('Adição de Cerveja Skol', '2024-05-15 14:45:00', 101, 2, 3), -- Maria Souza adicionou Cerveja Skol
-('Retirada de Arroz Tio João', '2024-05-20 10:15:00', 102, 3, 10), -- Carlos Pereira retirou Arroz Tio João
+('Entrada de Detergente OMO', '2024-07-10 08:30:00', 100, 1, 1), -- João Silva adicionou Detergente OMO
+('Entrada de Cerveja Skol', '2024-07-15 14:45:00', 101, 2, 3), -- Maria Souza adicionou Cerveja Skol
+('Saída de Arroz Tio João', '2024-07-20 10:15:00', 102, 3, 10), -- Carlos Pereira retirou Arroz Tio João
 */
 -- ============================================================================================================================================================
 INSERT INTO interacao_estoque (fk_produto, data_hora, fk_fechamento_estoque, categoria_interacao) VALUES
-(1, '2024-01-01 08:00:00', 1, 'Adição'),
-(2, '2024-01-02 10:30:00', 1, 'Adição'),
-(3, '2024-01-03 14:45:00', 1, 'Retirada'),
-(4, '2024-01-04 11:20:00', 1, 'Adição'),
-(5, '2024-01-05 09:15:00', 1, 'Retirada'),
-(6, '2024-01-06 12:00:00', 1, 'Adição'),
-(7, '2024-01-07 16:30:00', 1, 'Retirada'),
-(8, '2024-01-08 13:20:00', 1, 'Adição'),
-(9, '2024-01-09 10:45:00', 1, 'Adição'),
-(10, '2024-01-10 09:10:00', 1, 'Adição'),
-(11, '2024-01-11 11:00:00', 1, 'Retirada'),
-(12, '2024-01-12 14:25:00', 1, 'Adição'),
-(13, '2024-01-13 16:40:00', 1, 'Retirada'),
-(14, '2024-01-14 08:50:00', 1, 'Adição'),
-(15, '2024-01-15 10:15:00', 1, 'Retirada'),
-(16, '2024-01-16 12:30:00', 1, 'Adição'),
-(1, '2024-01-17 15:20:00', 1, 'Adição'),
-(2, '2024-01-18 13:35:00', 1, 'Retirada'),
-(3, '2024-01-19 09:55:00', 1, 'Adição'),
-(4, '2024-01-20 11:10:00', 1, 'Adição'),
-(5, '2024-01-21 10:25:00', 1, 'Retirada'),
-(6, '2024-01-22 12:45:00', 1, 'Adição'),
-(7, '2024-01-23 14:00:00', 1, 'Adição'),
-(8, '2024-01-24 16:20:00', 1, 'Retirada'),
-(9, '2024-01-25 08:40:00', 1, 'Adição'),
-(10, '2024-01-26 09:50:00', 1, 'Retirada'),
-(11, '2024-01-27 11:15:00', 1, 'Adição'),
-(12, '2024-01-28 13:00:00', 1, 'Adição'),
-(13, '2024-01-29 15:30:00', 1, 'Retirada'),
-(14, '2024-01-30 14:10:00', 1, 'Adição'),
-(15, '2024-01-31 16:40:00', 1, 'Retirada'),
-(1, '2024-02-01 08:20:00', 2, 'Adição'),
-(2, '2024-02-02 10:00:00', 2, 'Adição'),
-(3, '2024-02-03 11:45:00', 2, 'Retirada'),
-(4, '2024-02-04 12:30:00', 2, 'Adição'),
-(5, '2024-02-05 14:15:00', 2, 'Retirada'),
-(6, '2024-02-06 15:20:00', 2, 'Adição'),
-(7, '2024-02-07 16:10:00', 2, 'Adição'),
-(8, '2024-02-08 08:55:00', 2, 'Retirada'),
-(9, '2024-02-09 09:40:00', 2, 'Adição'),
-(10, '2024-02-10 10:25:00', 2, 'Adição'),
-(11, '2024-02-11 11:15:00', 2, 'Retirada'),
-(12, '2024-02-12 13:00:00', 2, 'Adição'),
-(13, '2024-02-13 14:45:00', 2, 'Retirada'),
-(14, '2024-02-14 15:30:00', 2, 'Adição'),
-(15, '2024-02-15 16:20:00', 2, 'Adição'),
-(16, '2024-02-16 08:10:00', 2, 'Retirada'),
-(1, '2024-02-17 09:50:00', 2, 'Adição'),
-(2, '2024-02-18 10:35:00', 2, 'Adição'),
-(3, '2024-02-19 11:25:00', 2, 'Retirada'),
-(4, '2024-02-20 13:05:00', 2, 'Adição'),
-(5, '2024-02-21 14:40:00', 2, 'Adição'),
-(6, '2024-02-22 15:15:00', 2, 'Retirada'),
-(7, '2024-02-23 16:00:00', 2, 'Adição'),
-(8, '2024-02-24 08:45:00', 2, 'Retirada'),
-(9, '2024-02-25 09:30:00', 2, 'Adição'),
-(10, '2024-02-26 10:20:00', 2, 'Adição'),
-(11, '2024-02-27 11:10:00', 2, 'Retirada');
+(1, '2024-08-01 08:00:00', 1, 'Entrada'),
+(2, '2024-08-02 10:30:00', 1, 'Entrada'),
+(3, '2024-08-03 14:45:00', 1, 'Saída'),
+(4, '2024-08-04 11:20:00', 1, 'Entrada'),
+(5, '2024-08-05 09:15:00', 1, 'Saída'),
+(6, '2024-08-06 12:00:00', 1, 'Entrada'),
+(7, '2024-08-07 16:30:00', 1, 'Saída'),
+(8, '2024-08-08 13:20:00', 1, 'Entrada'),
+(9, '2024-08-09 10:45:00', 1, 'Entrada'),
+(10, '2024-08-10 09:10:00', 1, 'Entrada'),
+(11, '2024-08-11 11:00:00', 1, 'Saída'),
+(12, '2024-08-12 14:25:00', 1, 'Entrada'),
+(13, '2024-08-13 16:40:00', 1, 'Saída'),
+(14, '2024-08-14 08:50:00', 1, 'Entrada'),
+(15, '2024-08-15 10:15:00', 1, 'Saída'),
+(16, '2024-08-16 12:30:00', 1, 'Entrada'),
+(1, '2024-08-17 15:20:00', 1, 'Entrada'),
+(2, '2024-08-18 13:35:00', 1, 'Saída'),
+(3, '2024-08-19 09:55:00', 1, 'Entrada'),
+(4, '2024-08-20 11:10:00', 1, 'Entrada'),
+(5, '2024-08-21 10:25:00', 1, 'Saída'),
+(6, '2024-08-22 12:45:00', 1, 'Entrada'),
+(7, '2024-08-23 14:00:00', 1, 'Entrada'),
+(8, '2024-08-24 16:20:00', 1, 'Saída'),
+(9, '2024-08-25 08:40:00', 1, 'Entrada'),
+(10, '2024-08-26 09:50:00', 1, 'Saída'),
+(11, '2024-08-27 11:15:00', 1, 'Entrada'),
+(12, '2024-08-28 13:00:00', 1, 'Entrada'),
+(13, '2024-08-29 15:30:00', 1, 'Saída'),
+(14, '2024-08-30 14:10:00', 1, 'Entrada'),
+(15, '2024-08-31 16:40:00', 1, 'Saída'),
+(1, '2024-09-01 08:20:00', 2, 'Entrada'),
+(2, '2024-09-02 10:00:00', 2, 'Entrada'),
+(3, '2024-09-03 11:45:00', 2, 'Saída'),
+(4, '2024-09-04 12:30:00', 2, 'Entrada'),
+(5, '2024-09-05 14:15:00', 2, 'Saída'),
+(6, '2024-09-06 15:20:00', 2, 'Entrada'),
+(7, '2024-09-07 16:10:00', 2, 'Entrada'),
+(8, '2024-09-08 08:55:00', 2, 'Saída'),
+(9, '2024-09-09 09:40:00', 2, 'Entrada'),
+(10, '2024-09-10 10:25:00', 2, 'Entrada'),
+(11, '2024-09-11 11:15:00', 2, 'Saída'),
+(12, '2024-09-12 13:00:00', 2, 'Entrada'),
+(13, '2024-09-13 14:45:00', 2, 'Saída'),
+(14, '2024-09-14 15:30:00', 2, 'Entrada'),
+(15, '2024-09-15 16:20:00', 2, 'Entrada'),
+(16, '2024-09-16 08:10:00', 2, 'Saída'),
+(1, '2024-09-17 09:50:00', 2, 'Entrada'),
+(2, '2024-09-18 10:35:00', 2, 'Entrada'),
+(3, '2024-09-19 11:25:00', 2, 'Saída'),
+(4, '2024-09-20 13:05:00', 2, 'Entrada'),
+(5, '2024-09-21 14:40:00', 2, 'Entrada'),
+(6, '2024-09-22 15:15:00', 2, 'Saída'),
+(7, '2024-09-23 16:00:00', 2, 'Entrada'),
+(8, '2024-09-24 08:45:00', 2, 'Saída'),
+(9, '2024-09-25 09:30:00', 2, 'Entrada'),
+(10, '2024-09-26 10:20:00', 2, 'Entrada'),
+(11, '2024-09-27 11:10:00', 2, 'Saída');
