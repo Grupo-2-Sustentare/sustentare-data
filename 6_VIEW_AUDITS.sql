61b877c89b93ee4705ceed3182f6a7d17263817c
Alexandra Harumi Yara <alexandra.yara@sptech.school>
2025-02-13 21:07:21 -0300

    Adição do uuid na view para exibição correta na api

--- 6_VIEW_AUDITS.sql
+++ 6_VIEW_AUDITS.sql
@@ -1,8 +1,8 @@
 DROP VIEW IF EXISTS audit_logs_view;
 
 CREATE VIEW audit_logs_view AS
-SELECT 'item_audit' AS tabela, descricao, data_hora, fk_usuario
+SELECT uuid() as 'id', 'item_audit' AS tabela, descricao, data_hora, fk_usuario
 FROM item_audit
 UNION ALL
-SELECT 'interacao_estoque_audit' AS tabela, descricao, data_hora, fk_usuario
+SELECT uuid() as 'id', 'interacao_estoque_audit' AS tabela, descricao, data_hora, fk_usuario
 FROM interacao_estoque_audit;	
